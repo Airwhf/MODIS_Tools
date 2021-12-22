5b45aec9e26a936183154e8a890eec686268ad12
cuitwhf <1450932912@qq.com>
2021-12-22 17:19:30 +0800

    Initial commit

---
+++ Modis数据重投影和拼接.py
@@ -0,0 +1,32 @@
+import pymodis
+import os
+
+# 设置modis输入路径
+input_dir = '/mnt/d/2020_MODIS_AOD/ModisLandCover'
+# 设置GTiff输出路径
+output_dir = '/mnt/d/2020_MODIS_AOD/ModisLandCover/output'
+# 设置输出分辨率
+res = 0.005
+# 设置波段
+subset = "( 1 )"
+
+# 建立file_list
+# file_list = []
+for sname in os.listdir(input_dir):
+    if sname[-4::] == '.hdf':
+        file_name = f'{input_dir}/{sname}'
+        # file_list.append(file_name)
+        convert = pymodis.convertmodis_gdal.convertModisGDAL(file_name, f'{output_dir}/{sname[17:23]}', subset, res,
+                                                             outformat='GTiff', epsg=4326, wkt=None,
+                                                             resampl='NEAREST_NEIGHBOR', vrt=False)
+        convert.run()
+
+cmd = f'gdal_merge.py -o mergeLandCover.tif {output_dir}/*'
+print(f'若需要对批处理结果进行拼接，在Linux终端使用命令：\n'
+      f'{cmd}')
+
+
+
+
+
+
